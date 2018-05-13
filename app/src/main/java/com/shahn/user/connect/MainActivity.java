9800a367aeb6f227768375946473f444afe63f5a
Neet Shah <neetshah02@gmail.com>
2018-05-13 00:22:48 -0400

    Assigned values to the fragments on create account acitivity.

--- app/src/main/java/com/shahn/user/connect/MainActivity.java
+++ app/src/main/java/com/shahn/user/connect/MainActivity.java
@@ -1,13 +1,35 @@
 package com.shahn.user.connect;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText email;
+    private EditText password;
+    private Button login;
+    private Button createacc;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        email = findViewById(R.id.edittext_email);
+        password = findViewById(R.id.pass);
+        login = findViewById(R.id.button_login);
+        createacc = findViewById(R.id.button_createacc);
+
+        createacc.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, createAccount.class));
+            }
+        });
     }
 }
