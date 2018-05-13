e3dbf1086b4f591671a9e482f258a43544e3daed
Neet Shah <neetshah02@gmail.com>
2018-05-13 12:31:39 -0400

    Create Account button linked to static method in firebaseAuth class user to create a new user.

--- app/src/main/java/com/shahn/user/connect/createAccount.java
+++ app/src/main/java/com/shahn/user/connect/createAccount.java
@@ -2,12 +2,37 @@ package com.shahn.user.connect;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.shahn.user.connect.firebase.firebaseAuth;
 
 public class createAccount extends AppCompatActivity {
+
+    private EditText email;
+    private EditText pass;
+    private String email_string;
+    private String pass_string;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_account);
+
+        Button createacc1;
+
+        createacc1 = findViewById(R.id.button_createacc);
+        email = findViewById(R.id.email);
+        pass = findViewById(R.id.pass);
+        createacc1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                email_string = email.getText().toString();
+                pass_string = pass.getText().toString();
+                firebaseAuth.createNewAccount(email_string, pass_string);
+            }
+        });
     }
 }
--- app/src/main/java/com/shahn/user/connect/firebase/firebaseAuth.java
+++ app/src/main/java/com/shahn/user/connect/firebase/firebaseAuth.java
@@ -1,5 +1,23 @@
 package com.shahn.user.connect.firebase;
 
+
+import android.support.annotation.NonNull;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 public class firebaseAuth {
+
+    private FirebaseAuth mAuth;
+
+    public static void createNewAccount(String email, String pass) {
+        FirebaseAuth mAuth1;
+        mAuth1 = FirebaseAuth.getInstance();
+        //mAuth1.createUserWithEmailAndPassword(); //add to this
+    }
 
 }
