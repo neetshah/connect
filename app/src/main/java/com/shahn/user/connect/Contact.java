e8d295006e76ecf9473bebb3537424ff0d24526d
Neet Shah <neetshah02@gmail.com>
2018-05-14 20:20:03 -0400

    Set up basic edittext and button and Firebase database dependencies.

--- app/src/main/java/com/shahn/user/connect/Contact.java
+++ app/src/main/java/com/shahn/user/connect/Contact.java
@@ -2,12 +2,19 @@ package com.shahn.user.connect;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class Contact extends AppCompatActivity {
+
+    private Button send;
+    private EditText msg;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contact);
+
+        send = findViewById(R.id.send);
     }
 }
