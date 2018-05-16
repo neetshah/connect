1fab1b30b5ccade67a1c32a1ece6110210223ce7
Neet Shah <neetshah02@gmail.com>
2018-05-15 20:52:55 -0400

    Added usernames/pass elements to the screen. Added initial code to parse the data from the edittexts.

--- app/src/main/java/com/shahn/user/connect/Register.java
+++ app/src/main/java/com/shahn/user/connect/Register.java
@@ -2,12 +2,41 @@ package com.shahn.user.connect;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class Register extends AppCompatActivity {
+
+    private EditText text;
+    private Button register;
+    private String username, pass, confirmpass;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                signUp();
+            }
+        });
     }
+
+    private void signUp() {
+        text = findViewById(R.id.username);
+        username = text.getText().toString();
+        text = findViewById(R.id.password);
+        pass = text.getText().toString();
+        text = findViewById(R.id.confirmpass);
+        confirmpass = text.getText().toString();
+
+        if(username.length() != 0 && pass.length() != 0 && confirmpass.length() != 0 &&
+                pass.equals(confirmpass)) {
+            ;
+        }
+    }
+
 }
